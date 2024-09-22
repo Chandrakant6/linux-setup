dad2fd69ac179359ba6f6f8d1fadd5ee491b78eb
chandu <141409100+Chandrakant6@users.noreply.github.com>
2024-09-22 10:30:46 +0530

    Update install.sh
    add install script to install brave broser

--- install.sh
+++ install.sh
@@ -2,3 +2,14 @@
 
 # update and upgrade
 sudo apt update && apt upgrade -y
+
+# install brave browser
+sudo apt install curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
